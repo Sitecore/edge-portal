8fe894ace89fef175d18713ddca6bf653dc6be21
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-03-10 10:22:43 -0800

    Moved navBar to its own compoenent and improved UI responsiveness on smaller screens.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,10 @@ import { LatestNewsAndArticles } from '../components/latestNewsAndArticles';
 import { CallToActionCards } from '../components/callToActionCards';
 import { ThreeVideoGrid } from '../components/threeVideoGrid';
 import { makeStyles } from '@material-ui/core/styles';
-import Image from 'next/image'
-import Grid from '@material-ui/core/Grid';
-import Link from 'next/link'
+import NavBar from '../components/navBar';
 
-const useStyles = makeStyles((theme) => ({
+
+const useStyles = makeStyles(() => ({
   title: {
     textAlign: 'center',
     color: "white"
@@ -21,96 +20,20 @@ const useStyles = makeStyles((theme) => ({
     padding: "5rem",
     borderRadius: "1rem",
     marginTop: "0px"
-  },
-  logoImage: {
-    maxWidth: "200px"
-  },
-  appBar: {
-    backgroundColor: "white",
-    margin: "0px",
-  },
-  navBarLink: {
-    color: "#9e9e9e",
-    padding: "10px",
-    '&:hover': {
-      color: "#616161"
-    }
-  },
-  gridItem: {
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    flexDirection: "column",
-    [theme.breakpoints.up('sm')]: {
-      flexDirection: "row"
-    },
-  },
-  navLogo: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    [theme.breakpoints.up('sm')]: {
-    },
-  },
-  link: {
-    textDecoration: "none",
   }
 }));
 
 
 
 export default function Home() {
-
   const classes = useStyles();
-
 
   return (
     <div>
       <Head>
       </Head>
       <Container maxWidth="lg">
-        <Grid container justify="center" spacing={1} className={classes.appBar}>
-          <Grid item md={2} xs={12} className={classes.navLogo}>
-            <Image src="/images/sitecore_logo.svg" width={200} height={100} />
-          </Grid>
-          <Grid item sm={10} xs={12} className={classes.gridItem}>
-            <Link href="#">
-              <a className={classes.link}>
-                <Typography className={classes.navBarLink} variant="h6" component="h1" gutterBottom>
-                  Getting Started
-              </Typography>
-              </a>
-            </Link>
-            <Link href="#">
-              <a className={classes.link}>
-                <Typography className={classes.navBarLink} variant="h6" component="h1" gutterBottom>
-                  Resources
-              </Typography>
-              </a>
-            </Link>
-            <Link href="#">
-              <a className={classes.link}>
-                <Typography className={classes.navBarLink} variant="h6" component="h1" gutterBottom>
-                  Docs
-              </Typography>
-              </a>
-            </Link>
-            <Link href="#">
-              <a className={classes.link}>
-                <Typography className={classes.navBarLink} variant="h6" component="h1" gutterBottom>
-                  Community
-              </Typography>
-              </a>
-            </Link>
-            <Link href="#">
-              <a className={classes.link}>
-                <Typography className={classes.navBarLink} variant="h6" component="h1" gutterBottom>
-                  Blog
-              </Typography>
-              </a>
-            </Link>
-          </Grid>
-        </Grid>
+        <NavBar />
         <Box my={4} className={classes.box}>
           <Typography className={classes.title} variant="h4" component="h1" gutterBottom>
             Sitecore Community
