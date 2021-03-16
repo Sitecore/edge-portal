1e919f12dc8ac1aa39f19944d088c719c826a3f0
Mark van Aalst <markvanaalst@gmail.com>
2021-03-16 23:59:29 +0100

    Included navBar Added div wrappers for styling to containers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
-
-import Head from 'next/head'
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import React from 'react';
-import { LatestNewsAndArticles } from '../components/latestNewsAndArticles';
-import { CallToActionCards } from '../components/callToActionCards';
-import { ThreeVideoGrid } from '../components/threeVideoGrid';
-import { makeStyles } from '@material-ui/core/styles';
-import { GetStaticProps } from 'next'
-import { Announcement } from '../interfaces/announcements'
-import { GetTopAnnouncements } from '../lib/announcements'
-import NavBar from '../components/navBar';
-import Footer from '../components/footer';
+import Head from "next/head";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import React from "react";
+import { LatestNewsAndArticles } from "../components/latestNewsAndArticles";
+import { CallToActionCards } from "../components/callToActionCards";
+import { ThreeVideoGrid } from "../components/threeVideoGrid";
+import { makeStyles } from "@material-ui/core/styles";
+import { GetStaticProps } from "next";
+import { Announcement } from "../interfaces/announcements";
+import { GetTopAnnouncements } from "../lib/announcements";
+import NavBar from "../components/navBar";
+import Footer from "../components/footer";
 
 const useStyles = makeStyles(() => ({
 	title: {
@@ -23,15 +22,35 @@ const useStyles = makeStyles(() => ({
 		backgroundColor: "#424242",
 		padding: "5rem",
 		borderRadius: "0",
+		marginTop: 0,
+		marginBottom: 0,
+	},
+	navBar: {
+		backgroundColor: "#fff",
+	},
+	ctaCards: {
+		backgroundColor: "#fff",
+		paddingTop: 64,
+		paddingBottom: 64,
+	},
+	grey: {
+		backgroundColor: "#f6f6f6",
+		marginTop: "20px",
 	},
 }));
 
-export default function Home({announcements} : {announcements: Announcement[]}) {
-  const classes = useStyles();
-    
+export default function Home({ announcements }: { announcements: Announcement[] }) {
+	const classes = useStyles();
+
 	return (
 		<div>
 			<Head></Head>
+
+			<div className={classes.navBar}>
+				<Container maxWidth="lg">
+					<NavBar />
+				</Container>
+			</div>
 
 			<Box my={4} className={classes.box}>
 				<Typography className={classes.title} variant="h4" component="h1" gutterBottom>
@@ -41,25 +60,29 @@ export default function Home({announcements} : {announcements: Announcement[]}) 
 					Get up and running quickly
 				</Typography>
 			</Box>
-			<Container maxWidth="md">
-				<CallToActionCards />
 
+			<div className={classes.ctaCards}>
+				<Container maxWidth="lg">
+					<CallToActionCards />
+				</Container>
+			</div>
+
+			<Container maxWidth="lg" className={classes.grey}>
 				<LatestNewsAndArticles announcements={announcements} />
-
 				<ThreeVideoGrid />
 			</Container>
-      
-      <Footer />
+
+			<Footer />
 		</div>
 	);
 }
-  
+
 export const getStaticProps: GetStaticProps = async () => {
-  let announcements = await GetTopAnnouncements(3)
-  return {
-      props: {
-          announcements: announcements
-      },
-      revalidate: 1
-  }
-}
+	let announcements = await GetTopAnnouncements(3);
+	return {
+		props: {
+			announcements: announcements,
+		},
+		revalidate: 1,
+	};
+};
