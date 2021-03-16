7141a337ac038d254581f521ae6c6b0b19caa934
Mark van Aalst <markvanaalst@gmail.com>
2021-03-16 21:27:19 +0100

    Use theme to manage colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import Theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>Living on the EDGE!</title>
-            </Head>
-
-            <Component {...pageProps} />
-        </>
-    )
+	return (
+		<>
+			<Head>
+				<title>Living on the EDGE!</title>
+			</Head>
+			<ThemeProvider theme={Theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
