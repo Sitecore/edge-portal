ec3f1e3bacd8005554c64314024eaadbd912ca23
Rob Earlam <robert.earlam@gmail.com>
2021-03-17 15:21:22 +1100

    Stubbed out docs colums

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -3,21 +3,32 @@ import React from "react";
 import NavBar from "../components/navBar";
 import Footer from "../components/footer";
 import HeroBanner from "../components/heroBanner"
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 
 export default function Docs() {
-    return (
-        <div>
+	return (
+		<div>
 			<Head>
 				<title>Documentation</title>
 			</Head>
 
-            <NavBar />
+			<NavBar />
 
-            <HeroBanner />
+			<HeroBanner />
 
-			<p>Content in here!</p>
+			<Box>
+				<Grid container justify="center" spacing={5}>
+					<Grid item xs={12} sm={6}>
+						<Box>This will be content hub</Box>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<Box>This will be Community Guides</Box>
+					</Grid>
+				</Grid>
+			</Box>
 
 			<Footer />
 		</div>
-    )
+	)
 }
