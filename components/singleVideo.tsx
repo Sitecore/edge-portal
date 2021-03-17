a79627bda01c6fe9d23ecc276171005504d09275
Mark van Aalst <markvanaalst@gmail.com>
2021-03-17 13:46:32 +0100

    Wrapped video grid in card component to add header

--- components/singleVideo.tsx
+++ components/singleVideo.tsx
@@ -1,50 +1,46 @@
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
-  videoCard: {
-    maxWidth: 250
-  },
-  media: {
-    height: 140,
-  },
+	videoCard: {
+		maxWidth: 250,
+	},
+	media: {
+		height: 140,
+	},
 }));
 
-
 export function SingleVideo() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Card className={classes.videoCard}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/images/play.png"
-          title="No Video Here" />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Video
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Video with video content in it with things people watch in videos.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-                    </Button>
-        <Button size="small" color="primary">
-          Learn More
-                    </Button>
-      </CardActions>
-    </Card>
-  );
+	return (
+		<Card className={classes.videoCard} square variant="outlined">
+			<CardActionArea>
+				<CardMedia className={classes.media} image="/images/play.png" title="No Video Here" />
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						Video
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						Video with video content in it with things people watch in videos.
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary">
+					Share
+				</Button>
+				<Button size="small" color="primary">
+					Learn More
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
--- components/threeVideoGrid.tsx
+++ components/threeVideoGrid.tsx
@@ -1,33 +1,61 @@
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import React from 'react';
-import { SingleVideo } from './singleVideo';
+import { CardContent, CardHeader } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+import React from "react";
+import { SingleVideo } from "./singleVideo";
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+	avatar: {
+		backgroundColor: theme.palette.primary.red,
+	},
+	header: {
+		borderBottom: "1px solid #f0f0f0",
+	},
+}));
 
 export function ThreeVideoGrid() {
-  return (
-    <Grid item xs={12}>
-      <Grid container justify="center" spacing={5}>
-        <Grid item>
-          <Box m={1}>
-            <SingleVideo />
-          </Box>
-        </Grid>
-        <Grid item>
-          <Box m={1}>
-            <SingleVideo />
-          </Box>
-        </Grid>
-        <Grid item>
-          <Box m={1}>
-            <SingleVideo />
-          </Box>
-        </Grid>
-        <Grid item>
-          <Box m={1}>
-          </Box>
-        </Grid>
-      </Grid>
-    </Grid>
-
-  );
+	const classes = useStyles();
+	return (
+		<Card square>
+			<CardHeader
+				avatar={
+					<Avatar aria-label="recipe" className={classes.avatar}>
+						E
+					</Avatar>
+				}
+				title="Latest videos"
+				subheader="VIDEO RESOURCES ABOUT THE SITECORE EXPERIENCE EDGE"
+				className={classes.header}
+			/>
+			<CardContent>
+				<Grid item xs={12}>
+					<Grid container justify="center" spacing={5}>
+						<Grid item>
+							<Box m={1}>
+								<SingleVideo />
+							</Box>
+						</Grid>
+						<Grid item>
+							<Box m={1}>
+								<SingleVideo />
+							</Box>
+						</Grid>
+						<Grid item>
+							<Box m={1}>
+								<SingleVideo />
+							</Box>
+						</Grid>
+						<Grid item>
+							<Box m={1}>
+								<SingleVideo />
+							</Box>
+						</Grid>
+					</Grid>
+				</Grid>
+			</CardContent>
+		</Card>
+	);
 }
