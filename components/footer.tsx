91ec73b1933355353f25fdb9a858b22975129e17
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-03-19 11:24:42 -0700

    Added link for footer copyright section.

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,8 @@ import { Box } from '@material-ui/core';
 const useStyles = makeStyles(() => ({
     title: {
         color: "white",
-        paddingBottom: "10px"
+        paddingBottom: "10px",
+        paddingLeft: ".5rem"
     },
     box: {
         backgroundColor: '#424242',
@@ -24,7 +25,10 @@ const useStyles = makeStyles(() => ({
     },
     footerLinkText: {
         color: "white",
-        padding: ".3rem"
+        paddingBottom: ".5rem",
+        paddingRight: ".5rem",
+        paddingLeft: ".5rem"
+
     },
     footerLink: {
         textDecoration: "none",
@@ -35,17 +39,19 @@ const useStyles = makeStyles(() => ({
     },
     copyright: {
         color: "white",
-        paddingTop: "5rem"
+        paddingTop: "5rem",
+        display: "flex",
     }
+
 
 }));
 
- interface FooterLinkData {
+interface FooterLinkData {
     LinkUrl: string,
     LinkText: string
 }
 
-export function FooterLink(props: FooterLinkData ) {
+export function FooterLink(props: FooterLinkData) {
     const classes = useStyles();
 
     return (
@@ -74,8 +80,8 @@ export default function Footer() {
                             <FooterLink LinkText="Sitecore Experience Platform" LinkUrl="https://www.sitecore.com/products/sitecore-experience-platform" />
                             <FooterLink LinkText="Sitecore Experience Commerce" LinkUrl="https://www.sitecore.com/products/sitecore-commerce" />
                             <FooterLink LinkText="Sitecore Content Hub" LinkUrl="https://www.sitecore.com/products/content-hub" />
-                            <FooterLink LinkText="Sitecore Experience Manager" LinkUrl="https://www.sitecore.com/products/experience-manager"/>
-                            <FooterLink LinkText="Personalization" LinkUrl="https://www.sitecore.com/products/sitecore-experience-platform/personalization"/>
+                            <FooterLink LinkText="Sitecore Experience Manager" LinkUrl="https://www.sitecore.com/products/experience-manager" />
+                            <FooterLink LinkText="Personalization" LinkUrl="https://www.sitecore.com/products/sitecore-experience-platform/personalization" />
                             <FooterLink LinkText="Marketing Automation" LinkUrl="https://www.sitecore.com/products/sitecore-experience-platform/marketing-automation" />
                             <FooterLink LinkText="Sitecore Omni" LinkUrl="https://www.sitecore.com/products/sitecore-experience-platform/headless" />
                             <FooterLink LinkText="Sitecore Cortex" LinkUrl="https://www.sitecore.com/products/sitecore-experience-platform/cortex" />
@@ -104,9 +110,16 @@ export default function Footer() {
                             <FooterLink LinkText="Trust center" LinkUrl="https://www.sitecore.com/trust" />
                         </Grid>
                     </Grid>
-                    <Typography className={classes.copyright} variant="body2">
-                        © Copyright 2020, Sitecore. All Rights Reserved | Legal | Privacy | Own the Experience® | webmaster@sitecore.net | +1-855-Sitecore
-                    </Typography>
+                    <Box className={classes.copyright}>
+                        <Typography variant="body2">
+                            © Copyright 2020, Sitecore. All Rights Reserved | 
+                        </Typography>
+                        <FooterLink LinkText="Legal" LinkUrl="https://www.sitecore.com/trust" />|
+                        <FooterLink LinkText="Privacy" LinkUrl="https://www.sitecore.com/trust/privacy-policy" />|
+                        <FooterLink LinkText="Own the Experience®" LinkUrl="https://www.sitecore.com/own-the-experience" />|
+                        <FooterLink LinkText="webmaster@sitecore.net" LinkUrl="mailto:webmaster@sitecore.net" />|
+                        <FooterLink LinkText="+1-855-Sitecore" LinkUrl="tel:+1-855-Sitecore" />
+                    </Box>
                 </Container>
             </Box>
         </div>
