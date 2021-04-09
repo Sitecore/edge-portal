b3f95f0073e11d359e68d382b0a3e3766ea2ec09
Mark van Aalst <markvanaalst@gmail.com>
2021-04-09 16:33:59 +0200

    Added temporary image to hero banner

--- components/heroBanner.tsx
+++ components/heroBanner.tsx
@@ -1,44 +1,36 @@
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import React from "react";
-import { HeroBannerData } from "interfaces/heroBannerData";
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { HeroBannerData } from 'interfaces/heroBannerData';
+import { blueGrey } from '@material-ui/core/colors';
 
 const useStyles = makeStyles(() => ({
-  title: {
-    textAlign: "center",
-    color: "white",
-  },
-  box: {
-    backgroundColor: "#424242",
-    padding: "5rem",
-    borderRadius: "0",
-    marginTop: 0,
-    marginBottom: 0,
-  },
+	title: {
+		textAlign: 'center',
+		color: 'white',
+	},
+	box: {
+		backgroundColor: '#424242',
+		backgroundImage: 'url(/img/jumbo-bg.png)',
+		padding: '5rem',
+		borderRadius: '0',
+		marginTop: 0,
+		marginBottom: 0,
+	},
 }));
 
 export default function HeroBanner({ data }: { data: HeroBannerData }) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Box my={4} className={classes.box}>
-      <Typography
-        className={classes.title}
-        variant="h4"
-        component="h1"
-        gutterBottom
-      >
-        {data.Title}
-      </Typography>
-      <Typography
-        className={classes.title}
-        variant="h5"
-        component="h1"
-        gutterBottom
-      >
-        {data.SubTitle}
-      </Typography>
-    </Box>
-  );
+	return (
+		<Box my={4} className={classes.box}>
+			<Typography className={classes.title} variant="h4" component="h1" gutterBottom>
+				{data.Title}
+			</Typography>
+			<Typography className={classes.title} variant="h5" component="h1" gutterBottom>
+				{data.SubTitle}
+			</Typography>
+		</Box>
+	);
 }
