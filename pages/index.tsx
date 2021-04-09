93b389cc390d66fb26df672eadfd732845b8abb6
Mark van Aalst <markvanaalst@gmail.com>
2021-04-09 15:07:36 +0200

    Changed home page hero banner title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,113 @@
-import Head from "next/head";
-import Container from "@material-ui/core/Container";
-import React from "react";
-import { LatestNewsAndArticles } from "../components/latestNewsAndArticles";
-import { CallToActionCards } from "../components/callToActionCards";
-import { ThreeVideoGrid } from "../components/threeVideoGrid";
-import { makeStyles } from "@material-ui/core/styles";
-import { GetStaticProps } from "next";
-import { Announcement } from "../interfaces/announcements";
-import { GetTopAnnouncements } from "../lib/announcements";
-import NavBar from "../components/navBar";
-import Footer from "../components/footer";
-import HeroBanner from "../components/heroBanner";
-import { HeroBannerData } from "../interfaces/heroBannerData";
-import { Blogpost } from "../interfaces/blogposts";
-import { GetLatestBlogposts } from "../lib/blogposts";
-import Box from "@material-ui/core/Box";
-import StackOverflow from "../components/stackOverflow";
-import { GetStackOverflowQuestionsByTag } from "../lib/stackOverflow";
-import { StackOverflowQuestion } from "../interfaces/stackOverflowQuestion";
-import { YouTubeVideo } from "../interfaces/youTubeVideo";
-import { GetYouTubeVideos } from "../lib/youtubeVideos";
+import Head from 'next/head';
+import Container from '@material-ui/core/Container';
+import React from 'react';
+import { LatestNewsAndArticles } from '../components/latestNewsAndArticles';
+import { CallToActionCards } from '../components/callToActionCards';
+import { ThreeVideoGrid } from '../components/threeVideoGrid';
+import { makeStyles } from '@material-ui/core/styles';
+import { GetStaticProps } from 'next';
+import { Announcement } from '../interfaces/announcements';
+import { GetTopAnnouncements } from '../lib/announcements';
+import NavBar from '../components/navBar';
+import Footer from '../components/footer';
+import HeroBanner from '../components/heroBanner';
+import { HeroBannerData } from '../interfaces/heroBannerData';
+import { Blogpost } from '../interfaces/blogposts';
+import { GetLatestBlogposts } from '../lib/blogposts';
+import Box from '@material-ui/core/Box';
+import StackOverflow from '../components/stackOverflow';
+import { GetStackOverflowQuestionsByTag } from '../lib/stackOverflow';
+import { StackOverflowQuestion } from '../interfaces/stackOverflowQuestion';
+import { YouTubeVideo } from '../interfaces/youTubeVideo';
+import { GetYouTubeVideos } from '../lib/youtubeVideos';
 
 const useStyles = makeStyles((theme) => ({
-  ctaCards: {
-    backgroundColor: theme.palette.common.white,
-    paddingTop: 64,
-    paddingBottom: 64,
-  },
-  grey: {
-    backgroundColor: theme.palette.grey[100],
-    marginTop: "20px",
-    marginBottom: "20px",
-  },
-  white: {
-    backgroundColor: theme.palette.common.white,
-    paddingTop: "20px",
-    paddingBottom: "20px",
-  },
+	ctaCards: {
+		backgroundColor: theme.palette.common.white,
+		paddingTop: 64,
+		paddingBottom: 64,
+	},
+	grey: {
+		backgroundColor: theme.palette.grey[100],
+		marginTop: '20px',
+		marginBottom: '20px',
+	},
+	white: {
+		backgroundColor: theme.palette.common.white,
+		paddingTop: '20px',
+		paddingBottom: '20px',
+	},
 }));
 
 export default function Home({
-  announcements,
-  blogposts,
-  heroBannerData,
-  stackOverflowData,
-  youTubeData,
+	announcements,
+	blogposts,
+	heroBannerData,
+	stackOverflowData,
+	youTubeData,
 }: {
-  announcements: Announcement[];
-  heroBannerData: HeroBannerData;
-  blogposts: Blogpost[];
-  stackOverflowData: StackOverflowQuestion[];
-  youTubeData: YouTubeVideo[];
+	announcements: Announcement[];
+	heroBannerData: HeroBannerData;
+	blogposts: Blogpost[];
+	stackOverflowData: StackOverflowQuestion[];
+	youTubeData: YouTubeVideo[];
 }) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div>
-      <Head></Head>
+	return (
+		<div>
+			<Head></Head>
 
-      <NavBar />
+			<NavBar />
 
-      <HeroBanner data={heroBannerData} />
+			<HeroBanner data={heroBannerData} />
 
-      <div className={classes.ctaCards}>
-        <Container maxWidth="lg">
-          <CallToActionCards />
-        </Container>
-      </div>
+			<div className={classes.ctaCards}>
+				<Container maxWidth="lg">
+					<CallToActionCards />
+				</Container>
+			</div>
 
-      <Container maxWidth="lg" className={classes.grey}>
-        <LatestNewsAndArticles
-          announcements={announcements}
-          blogposts={blogposts}
-        />
-        <ThreeVideoGrid videos={youTubeData} />
-      </Container>
+			<Container maxWidth="lg" className={classes.grey}>
+				<LatestNewsAndArticles announcements={announcements} blogposts={blogposts} />
+				<ThreeVideoGrid videos={youTubeData} />
+			</Container>
 
-      <Box width={1} className={classes.white}>
-        <Container maxWidth="lg" className={classes.white}>
-          <StackOverflow questions={stackOverflowData} />
-        </Container>
-      </Box>
-      <Footer />
-    </div>
-  );
+			<Box width={1} className={classes.white}>
+				<Container maxWidth="lg" className={classes.white}>
+					<StackOverflow questions={stackOverflowData} />
+				</Container>
+			</Box>
+			<Footer />
+		</div>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const announcements = await GetTopAnnouncements(3);
-  const blogposts = await GetLatestBlogposts(4);
-  const stackOverflowQuestions: StackOverflowQuestion[] = await GetStackOverflowQuestionsByTag(
-    "jss"
-  );
-  const youTubeVideos: YouTubeVideo[] = await GetYouTubeVideos();
+	const announcements = await GetTopAnnouncements(3);
+	const blogposts = await GetLatestBlogposts(4);
+	const stackOverflowQuestions: StackOverflowQuestion[] = await GetStackOverflowQuestionsByTag('jss');
+	const youTubeVideos: YouTubeVideo[] = await GetYouTubeVideos();
 
-  // Pushing video onto array 4 times so we have more data. Currently Playlist only has one video
-  if (youTubeVideos.length > 0) {
-    youTubeVideos.push(youTubeVideos[0]);
-    youTubeVideos.push(youTubeVideos[0]);
-    youTubeVideos.push(youTubeVideos[0]);
-  }
+	// Pushing video onto array 4 times so we have more data. Currently Playlist only has one video
+	if (youTubeVideos.length > 0) {
+		youTubeVideos.push(youTubeVideos[0]);
+		youTubeVideos.push(youTubeVideos[0]);
+		youTubeVideos.push(youTubeVideos[0]);
+	}
 
-  const heroBannerData: HeroBannerData = {
-    Title: "Sitecore Community",
-    SubTitle: "Get up and running quickly",
-  };
-  return {
-    props: {
-      announcements: announcements,
-      heroBannerData: heroBannerData,
-      stackOverflowData: stackOverflowQuestions,
-      youTubeData: youTubeVideos,
-      blogposts: blogposts,
-    },
-    revalidate: 1,
-  };
+	const heroBannerData: HeroBannerData = {
+		Title: 'Sitecore Experience Edge Portal',
+		SubTitle: 'Get up and running quickly',
+	};
+	return {
+		props: {
+			announcements: announcements,
+			heroBannerData: heroBannerData,
+			stackOverflowData: stackOverflowQuestions,
+			youTubeData: youTubeVideos,
+			blogposts: blogposts,
+		},
+		revalidate: 1,
+	};
 };
