b0d179c3b0eec0d510338c9ad9d205af55844682
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-04-12 08:33:52 -0700

    Added link to youtube videos. Removed share & learn more buttons.

--- components/singleVideo.tsx
+++ components/singleVideo.tsx
@@ -4,10 +4,9 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import { YouTubeVideo } from "../interfaces/youTubeVideo";
+import Link from "next/link";
 
 const useStyles = makeStyles(() => ({
   videoCard: {
@@ -21,9 +20,12 @@ const useStyles = makeStyles(() => ({
 export function SingleVideo(video: any) {
   const classes = useStyles();
   const youTubeVideo: YouTubeVideo = video.video;
+  const youTubeVideoUrlPrefix = "https://www.youtube.com/watch?v="
+  const youTubeLink = `${youTubeVideoUrlPrefix}${youTubeVideo.snippet.resourceId.videoId}`;
 
   return (
     <Card className={classes.videoCard} square variant="outlined">
+      <Link href={youTubeLink}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
@@ -39,14 +41,7 @@ export function SingleVideo(video: any) {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
+      </Link>
     </Card>
   );
 }
